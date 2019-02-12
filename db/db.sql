4d3597acf6091d482d713e2033b11fa6b2698a3b
Ismail Arafa <ismailarafa18@gmail.com>
2019-02-12 14:20:23 -0500

    implement http-basic authentication

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
-  username TEXT UNIQUE
+  username TEXT UNIQUE,
+  password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS exercises (
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcrypt");
 const knex = require("../db");
 
 const router = express.Router();
@@ -9,7 +10,7 @@ const jsonParser = bodyParser.json({ type: "application/json" });
 const isValidDate = date => {
   const d = new Date(date);
   return !isNaN(d.getTime());
-}
+};
 
 router.post(
   "/users",
@@ -18,11 +19,19 @@ router.post(
     check("username")
       .isLength({ min: 1 })
       .trim()
+      .escape(),
+    check("password")
+      .isLength({ min: 8 })
+      .trim()
       .escape()
   ],
   async (req, res) => {
     if (!req.body.username) {
       return res.status(400).json({ error: "No username found" });
+    }
+
+    if (!req.body.password) {
+      return res.status(400).json({ error: "No password found" });
     }
 
     const errors = validationResult(req);
@@ -31,7 +40,11 @@ router.post(
     }
 
     try {
-      const result = await knex.raw("INSERT INTO users (username) values (?) RETURNING username", req.body.username);
+      const password = await bcrypt.hash(req.body.password, 10);
+      const result = await knex.raw("INSERT INTO users (username, password) values (?, ?) RETURNING username", [
+        req.body.username,
+        password
+      ]);
       const { username } = result.rows[0];
       res.status(200).json({ msg: `Username ${username} created.` });
     } catch (e) {
@@ -41,7 +54,7 @@ router.post(
 );
 
 router.post(
-  "/users/:userId/exercises",
+  "/users/exercises",
   jsonParser,
   [
     check("description")
@@ -57,31 +70,43 @@ router.post(
       .escape()
   ],
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ error: "Invalid input data" });
+    if (!req.headers.authorization) {
+      return res.status(400).json({ error: "Authorization credentials not found" });
     }
-
-    if (!isValidDate(req.body.date) && req.body.date) {
-      return res.status(422).json({ error: "Invalid date" });
-    }
-
-    const isValidId = /^\d+$/.test(req.params.userId);
-    if (!isValidId) {
-      return res.status(422).json({ error: "Invalid user ID" });
-    }
-
+    const credentials = req.headers.authorization.split(" ")[1];
+    const [username, password] = credentials.split(":");
     try {
-      const result = await knex("exercises")
+      const resp = await knex("users")
+        .select("*")
+        .where("username", username);
+
+      if (resp.length === 0) {
+        return res.status(400).json({ error: "Invalid username or user does not exist" });
+      }
+
+      const isValidPassword = await bcrypt.compare(password, resp[0].password);
+      if (!isValidPassword) {
+        return res.status(400).json({ error: "Wrong password for username" });
+      }
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ error: "Invalid input data" });
+      }
+
+      if (!isValidDate(req.body.date) && req.body.date) {
+        return res.status(422).json({ error: "Invalid date" });
+      }
+
+      await knex("exercises")
         .insert({
           description: req.body.description,
-          user_id: req.params.userId,
+          user_id: resp[0].id,
           duration: req.body.duration,
           date: req.body.date || null
         })
         .returning("user_id");
-      const userId = result[0];
-      res.status(200).json({ error: `Exercise created for user with id ${userId}` });
+      res.status(200).json({ error: `Exercise created for user ${resp[0].username}` });
     } catch (e) {
       console.log(e);
       res.status(400).json({ error: "Could not create exercise." });
@@ -89,25 +114,38 @@ router.post(
   }
 );
 
-router.get("/users/:userId/exercises?", async (req, res) => {
-  const isValidId = /^\d+$/.test(req.params.userId);
-  if (!isValidId) {
-    return res.status(422).json({ error: "Invalid user ID" });
-  }
-
-  if (req.query.from && !isValidDate(req.query.from)) {
-    return res.status(422).json({ error: "Invalid start date" });
-  }
-
-  if (req.query.to && !isValidDate(req.query.to)) {
-    return res.status(422).json({ error: "Invalid end date" });
+router.get("/users/exercises?", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(400).json({ error: "Authorization credentials not found" });
   }
 
   try {
+    const credentials = req.headers.authorization.split(" ")[1];
+    const [username, password] = credentials.split(":");
+    const resp = await knex("users")
+      .select("*")
+      .where("username", username);
+    if (resp.length === 0) {
+      return res.status(400).json({ error: "Invalid username or user does not exist" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, resp[0].password);
+    if (!isValidPassword) {
+      return res.status(400).json({ error: "Wrong password for username" });
+    }
+
+    if (req.query.from && !isValidDate(req.query.from)) {
+      return res.status(422).json({ error: "Invalid start date" });
+    }
+
+    if (req.query.to && !isValidDate(req.query.to)) {
+      return res.status(422).json({ error: "Invalid end date" });
+    }
+
     const query = knex("exercises")
       .select("users.username", "exercises.description", "exercises.duration", "exercises.date")
       .join("users", "users.id", "=", "exercises.user_id")
-      .where("exercises.user_id", "=", req.params.userId);
+      .where("exercises.user_id", "=", resp[0].id);
 
     if (req.query.from) {
       query.andWhere("date", ">=", req.query.from);
